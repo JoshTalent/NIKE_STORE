f2c71f25d0bbc4637ba6dce3335a1f2815df6680
ntwari josue <134047313+JoshTalent@users.noreply.github.com>
2026-03-20 12:02:59 +0200

    Update Footer component with new layout and copyright

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,6 +5,7 @@ import { IFooterAPI } from '../data/data'
 const Footer:React.FC<IFooterAPI> = ({titles, links}) => {
   return (
     <>
+      
       <footer className='bg-theme pt-7 pb-5'>
         <div className='nike-container text-slate-200'>
           <div className='grid items-start grid-cols-3 max-w-2xl w-full m-auto 
@@ -15,6 +16,7 @@ const Footer:React.FC<IFooterAPI> = ({titles, links}) => {
                 >{val.title}</h1>
               </div>
             ))}
+            
             {links.map((list, i) => (
               <ul key={i} className="grid items-center gap-1">
                 {
@@ -25,6 +27,7 @@ const Footer:React.FC<IFooterAPI> = ({titles, links}) => {
               </ul>
             ))}
           </div>
+          
           <div className='mt-5 text-center'>
                 <p className='text-sm md:text-center'>Copyright <sup className='text-base
                 font-bold'>&copy;</sup> All Reserved Rights 2022 <span className='font-semibold'>
