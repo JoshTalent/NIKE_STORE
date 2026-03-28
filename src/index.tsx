ea2bdde1b966a332f02fc5e53300339665acaa20
ntwari josue <134047313+JoshTalent@users.noreply.github.com>
2026-03-28 18:40:42 +0200

    Add import for Toaster component

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import { Toaster } from 'react-hot-toast';
 
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
