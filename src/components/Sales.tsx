32f4cd85a82478b2e712c9d069ae907164a37ab9
ntwari josue <134047313+JoshTalent@users.noreply.github.com>
2026-04-18 20:29:55 +0200

    Fix syntax error in Sales component JSX

--- src/components/Sales.tsx
+++ src/components/Sales.tsx
@@ -6,7 +6,8 @@ import Title from './utils/Title'
 const Sales:React.FC< ITopRatesSlaes> = ({items,ifExists,title}) => {
   return (
     <>
-      <div className='nike-container'>
+      <div className='nike-container'>]
+        
         <Title title={title}/>
         <div className={`grid items-center justify-items-center grid-cols-4 xl:grid-cols-3 
         md:grid-cols-2 sm:grid-cols-1 gap-7 lg:gap-5 mt-7
