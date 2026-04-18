9ae68fdca38c08b5f26597c1a216d5dfedff80fb
ntwari josue <134047313+JoshTalent@users.noreply.github.com>
2026-04-18 20:30:13 +0200

    Fix grid class names and clean up JSX structure

--- src/components/Sales.tsx
+++ src/components/Sales.tsx
@@ -7,7 +7,6 @@ const Sales:React.FC< ITopRatesSlaes> = ({items,ifExists,title}) => {
   return (
     <>
       <div className='nike-container'>]
-        
         <Title title={title}/>
         <div className={`grid items-center justify-items-center grid-cols-4 xl:grid-cols-3 
         md:grid-cols-2 sm:grid-cols-1 gap-7 lg:gap-5 mt-7
