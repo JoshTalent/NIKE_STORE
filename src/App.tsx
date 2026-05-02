dcc30b3fce4d877ffa30246bb02d4c32cb0bbbb3
ntwari josue <134047313+JoshTalent@users.noreply.github.com>
2026-05-02 11:45:08 +0200

    Add empty line for code formatting in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Cart, FlexContent, Footer, Hero, Navbar, Sales, Stories } from "./components";
 import heroapi, { popularsales, toprateslaes, highlight, sneaker, story, footerAPI } from './data/data'
 
+
 function App() {
 
   return (
